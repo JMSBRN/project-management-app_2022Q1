e27e46cb4f4ab180c35d5462567de8a25278a492
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-05-10 00:33:01 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './containers/layout/Layout';
+import Header from './containers/header/Header';
+import Welcome from './pages/welcome/Welcome';
+import Main from './pages/main/Main';
+import Board from './pages/board/Board';
+import Footer from './containers/footer/Footer';
+import NotFound from './pages/notFound/NotFound';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route index element={<Welcome />} />
+          <Route path="main" element={<Main />} />
+          <Route path="board" element={<Board />} />
+          <Route path="notFound" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/board/Board.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './board.css';
+
+export default class Board extends Component {
+  render() {
+    return <div>Board</div>;
+  }
+}
