f090b7420f54956101c39387a81d63ba9797a223
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-05-10 11:32:59 +0300

    feat: add NavLinks in Header comp.

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './header.css';
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <div className="header">
+        <ul>
+          <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'/'}>
+            Welcome
+          </NavLink>
+        </ul>
+        <ul>
+          <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'/main'}>
+            Main
+          </NavLink>
+        </ul>
+        <ul>
+          <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'/board'}>
+            Board
+          </NavLink>
+        </ul>
+      </div>
+    );
   }
 }
