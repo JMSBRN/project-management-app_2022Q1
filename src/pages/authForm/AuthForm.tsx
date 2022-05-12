895998ce43bcb16b1a6de4172bc07abc413450ee
Irina <irenserge@mail.ru>
2022-05-12 16:40:16 +0300

    fix: fix bag

--- src/pages/authForm/AuthForm.tsx
+++ src/pages/authForm/AuthForm.tsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import './authForm.css';
-import Modal from '../../components/modal/Modal';
 
 export class AuthForm extends Component {
   render() {
-    return <div>
-      <Modal />
-      AuthForm</div>;
+    return <div>AuthForm</div>;
   }
 }
