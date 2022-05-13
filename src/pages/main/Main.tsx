d167ef1ea291884029d4cc62e153d5632de60977
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-05-13 12:50:36 +0300

    feat:  create Bord comp., add temporary link  to Main board in Auth comp.

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import Board from '../../components/board/Board';
 import './main.css';
 
 export default class Main extends Component {
   render() {
-    return <div>Main</div>;
+    return (
+      <div>
+        <div className="boards-wrapper">
+          <Board boardTitle={'work_one'} boardDescription={'frontend'} />
+        </div>
+      </div>
+    );
   }
 }
