f89775b2c49f08f62facba3c7134365d08849447
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-05-27 22:49:51 +0300

    fix: update styles in LoginForm, AuthForm

--- src/pages/authPage/AuthPage.tsx
+++ src/pages/authPage/AuthPage.tsx
@@ -5,6 +5,7 @@ import * as Styled from './authPage.style';
 const AuthPage = () => {
   return (
     <Styled.Auth>
+      <Styled.Main_title>Plase fill this out all fields...</Styled.Main_title>
       <AuthForm />
     </Styled.Auth>
   );
--- src/pages/loginPage/LoginPage.style.ts
+++ src/pages/loginPage/LoginPage.style.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-export const Auth = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
 export const Main_title = styled.h2`
   margin-top: 50px;
   text-align: center;
