b56211f9de39d63846a23ecbdbbf6cdb5a1af04c
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-05-30 16:00:46 +0300

    feat: task: 61.Displays the boards as a list. Done.

--- src/pages/main/Main.style.ts
+++ src/pages/main/Main.style.ts
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
+export const Main = styled.div`
+  width: 100%;
+  height: auto;
+  position: relative;
+`;
 export const Boards_list = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 20px;
   width: 100%;
   height: auto;
   display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
 `;
